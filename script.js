288b5ab25da8f7e44254eb93d55dc9a11dc50b08
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-18 09:36:43 +0530

    add

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
-
+    var firstName = document.forms["form1"]["fname"].value;
+    var lastName = document.forms["form1"]["lname"].value;
+  
+    // Display the full name using alert()
+    alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
+    
+    // Prevent form submission
+    return false;
 }
