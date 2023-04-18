a754edf9e516044e0c81ddd419b408a10be921db
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-18 09:44:47 +0530

    add

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 function getFormvalue() {
-    var firstName = document.forms["form1"]["fname"].value;
-    var lastName = document.forms["form1"]["lname"].value;
+    // var firstName = document.forms["form1"]["fname"].value;
+    // var lastName = document.forms["form1"]["lname"].value;
   
-    // Display the full name using alert()
-    alert('First Name: ' + firstName + 'Last Name: ' + lastName);
+    // // Display the full name using alert()
+    // alert('First Name: ' + firstName + 'Last Name: ' + lastName);
     
-    // Prevent form submission
-    return false;
-}
+    // // Prevent form submission
+    // return false;
+
+        var firstName = document.forms["form1"]["fname"].value;
+        var lastName = document.forms["form1"]["lname"].value;
+        var middleName = document.forms["form1"]["mname"].value; // Add middle name
+    
+        // Display the full name using alert()
+        var fullName = 'First Name: ' + firstName;
+        if (middleName) {
+            fullName += ' Middle Name: ' + middleName; // Include middle name if provided
+        }
+        fullName += ' Last Name: ' + lastName;
+        alert(fullName);
+    
+        // Prevent form submission
+        return false;
+    }
+    
